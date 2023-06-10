a4bff0c3d76e5f7b17d5bad429fa8155aa3662c2
smac4343 <smac4343@gmail.com>
2023-06-10 23:26:51 +0300

    Add ready programm

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+import src.SalesManager;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] salesData = {5, 12, 20, 8, 15, 30, 12, 18};
+        SalesManager salesManager = new SalesManager(salesData);
+
+        int maxSale = salesManager.max();
+        System.out.println("The maximum sale is: " + maxSale);
+    }
+}
